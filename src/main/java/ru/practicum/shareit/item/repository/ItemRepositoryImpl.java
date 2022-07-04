d332ba1587a839b6a1f166b9250882989b8c6537
AlexanderGrechishnikov <shurik-05-00@ya.ru>
2022-07-04 14:07:27 +0300

    refactor: code style

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepositoryImpl.java
@@ -7,7 +7,6 @@ import ru.practicum.shareit.utill.exceptions.NotFoundException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Set;
 import java.util.stream.Collectors;
 
